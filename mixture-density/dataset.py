a2cbfe50d50c031de5f71d9dd6ce253219e4b768
talha local <mntalha.kilic@hotmail.com>
2022-12-25 23:08:08 -0600

    Mixture Density Network Added
    Signed-off-by: talha local <mntalha.kilic@hotmail.com>

--- mixture-density/dataset.py
+++ mixture-density/dataset.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Dec  12, 2022
+Created on Sat Dec 24 22:55:13 2022
 
 @author: talha.kilic
 """
@@ -19,14 +20,14 @@ import h5py
 root_dir = "../dataset/dataset_z9.hdf5" 
 
 
-class MicrostructureDataset(Dataset):
+class LatentVectorDataset(Dataset):
     
     def __init__(self, path):
         
         
         dataset = h5py.File(path, 'r')
         
-        self.images = dataset["x"]  # 100x96x96 
+        # self.images = dataset["x"]  # 100x96x96 
         
         self.optical_absorbtion = dataset["y"]  #100x1
 
@@ -34,28 +35,16 @@ class MicrostructureDataset(Dataset):
 
         
     def __len__(self):
-        return len(self.images)  
+        return len(self.optical_absorbtion)  
 
     def __getitem__(self,idx):
         
-        return self.images[idx]
+        return self.optical_absorbtion[idx], self.latent_vectors[idx]
     
     
-    def visualize(self, img):
-        
-        import matplotlib.pyplot as plt
-        from skimage import filters
-
-        plt.figure(0)
-        gray=img
-        val = filters.threshold_otsu(gray)
-        plt.imshow(gray>val, cmap='gray')
-        print ("====> image!")
-
-        
 if __name__ == "__main__":
     
-    dataset = MicrostructureDataset(root_dir)
+    dataset = LatentVectorDataset(root_dir)
     
     ratio = list(map(int,[0.7*len(dataset), 0.2*len(dataset), 0.1*len(dataset)]))
     train, test ,validation = random_split(dataset, ratio)
--- mixture-density/model.py
+++ mixture-density/model.py
@@ -16,74 +16,69 @@ OUTPUT_DIMS = 4 # output dimension
 NUM_SAMPLE = 30 # number of sampled points for each input y
 #Paths
 
+
 class DensityNetwork(nn.Module):
 
         def __init__(self):
             super(DensityNetwork, self).__init__()
-            
-            
+                        
             self.enc1 = nn.Linear(in_features=1, out_features=16)
-            self.batch1=nn.BatchNorm1d(num_features=1) 
+            self.batch1=nn.BatchNorm1d(num_features=16) 
             self.relu1 = nn.ReLU()
             
             #concat
-            
             self.enc2 = nn.Linear(in_features=17, out_features=16)
             self.batch2=nn.BatchNorm1d(16) 
             self.relu2= nn.ReLU()
             
-            
             #concat
-            
             self.enc3 = nn.Linear(in_features=33, out_features=16)
             self.batch3=nn.BatchNorm1d(16) 
             self.relu3= nn.ReLU()  
             
-            
             #concat
-            
-            self.enc4 = nn.Linear(in_features=33, out_features=16)
+            self.enc4 = nn.Linear(in_features=49, out_features=16)
             self.batch4=nn.BatchNorm1d(16) 
             self.relu4= nn.ReLU()
             
-            
-            
             self.mdn_mus = nn.Linear(in_features = 16, out_features=N_MIXES*OUTPUT_DIMS)
-            self.mdn_sigmas = nn.Linear(in_features = 16, out_features=N_MIXES*OUTPUT_DIMS)  #activation=elu_plus_one_plus_epsilon)
+            self.mdn_sigmas = nn.Linear(in_features = 16, out_features=N_MIXES*OUTPUT_DIMS)
+            self.leaky_relu = nn.LeakyReLU(0.1)
             self.mdn_pi = nn.Linear(in_features = 16, out_features=N_MIXES)
             
         def forward(self, x):
 
-            # encoding
-            x = x.reshape(-1,1,1)
             y_val = x.clone()
             x = self.enc1(x)
             x = self.batch1(x)            
             x = self.relu1(x)
             
-            concat = torch.cat([x,y_val],2)
+            concat = torch.cat([x,y_val],1)
             x = self.enc2(concat)
             x = self.batch2(x)            
             x = self.relu2(x)
             
-            concat = torch.cat(x,concat)
+            concat = torch.cat([x,concat],1)
             x = self.enc3(concat)
             x = self.batch3(x)            
             x = self.relu3(x)
             
+            concat = torch.cat([x,concat],1)
+            x = self.enc4(concat)
+            x = self.batch4(x)            
+            x = self.relu4(x)
             
             mdn_mus = self.mdn_mus(x)
-            mdn_sigmas = self.mdn_sigmas(x)
+            mdn_sigmas = self.leaky_relu(self.mdn_sigmas(x))
             mdn_pi = self.mdn_pi(x)
             
-            output = torch.cat([mdn_mus, mdn_sigmas, mdn_pi])
+            output = torch.cat([mdn_mus, mdn_sigmas, mdn_pi],1)
         
             return output
 
-
 if __name__ == "__main__":
     model = DensityNetwork()
-    model.train()
-    val = torch.tensor(0.5)
+    #model.train()
+    val = torch.tensor(torch.rand(4,1)) 
     output = model(val)
     print(model.parameters)
